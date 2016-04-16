5402aff285bed0bac83d7b147978feb2803eb988
jdev6 <jd6.dev@gmail.com>
2016-04-16 17:51:46 +0200

    Update classic.lua

--- classic.lua
+++ classic.lua
@@ -5,9 +5,6 @@ local classic = {
     _LICENSE     = [[
         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
         Version 2, December 2004
-
-        Copyright (C) 2016 jdev6
-
         Everyone is permitted to copy and distribute verbatim or modified
         copies of this license document, and changing it is allowed as long
         as the name is changed.
